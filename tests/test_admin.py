cefd90704ef44a128e21c364b626fcd051115c30
Vivien Henry <vivien.henry@inductivebrain.fr>
2026-04-12 22:44:47 +0200

    Squashed 'freecad/plume/pysvn/' content from commit 7e0b8da
    git-subtree-dir: freecad/plume/pysvn
    git-subtree-split: 7e0b8da34a059a1fd225321c7e263c232904367c

---
+++ tests/test_admin.py
@@ -0,0 +1,46 @@
+__author__ = 'dsoprea'
+
+import unittest
+import tempfile
+import shutil
+import logging
+
+import svn.admin
+import svn.remote
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class TestAdmin(unittest.TestCase):
+    """
+    For testing svn/admin.py
+    """
+
+    def shortDescription(self):
+        return None
+
+    def test_create_repository(self):
+        """
+        Testing repository creation.
+        :return:
+        """
+
+        temp_path = tempfile.mkdtemp()
+        shutil.rmtree(temp_path)
+
+        a = svn.admin.Admin()
+
+        try:
+            # Create.
+            a.create(temp_path)
+
+            # Do a read.
+            rc = svn.remote.RemoteClient('file://' + temp_path)
+            info = rc.info()
+            _LOGGER.debug("Info from new repository: [%s]", str(info))
+        finally:
+            try:
+                shutil.rmtree(temp_path)
+            except:
+                pass
+
